16a5ac7dec2892340503694b8da966ef7264eb48
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2023-04-28 01:20:50 -0300

    Criando prepareStatement

---
+++ Programa2/Main.java
@@ -0,0 +1,49 @@
+package Programa2;
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try{
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                    "INSERT INTO seller "
+                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+                    + "VALUES"
+                    + "(?, ?, ?, ?, ?)");
+            st.setString(1, "Carl Purple");
+            st.setString(2,"carl@gmail.com");
+            st.setDate(3,new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+            st.setDouble(4, 3000.0);
+            st.setInt(5, 4);
+
+           int rowsAffected = st.executeUpdate();
+
+            System.out.println("pronto! Linhas afetadas: " + rowsAffected);
+        }
+
+        catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+
+        }
+    }
+}
